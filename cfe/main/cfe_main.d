7e4ac1c5f3c02dedf5d478313cfd779243055ab5
Ed <73446118+ezouu@users.noreply.github.com>
2024-06-24 12:04:05 -0700

    Add files via upload

---
+++ cfe/main/cfe_main.d
@@ -0,0 +1,46 @@
+cfe-master/cfe/main/cfe_main.o: \
+ /Users/easy/Downloads/cfe-master/cfe/main/cfe_main.c \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/lib_types.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/lib_queue.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/lib_string.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/lib_malloc.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/lib_printf.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/lib_hssubr.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_iocb.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_device.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_ioctl.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_error.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_console.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_timer.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_devfuncs.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_cache.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_main.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/env_subr.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/ui_command.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/cfe_mem.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/exception.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/segtable.h \
+ /Users/easy/Downloads/cfe-master/cfe/include/initdata.h
+/Users/easy/Downloads/cfe-master/cfe/include/cfe.h:
+/Users/easy/Downloads/cfe-master/cfe/include/lib_types.h:
+/Users/easy/Downloads/cfe-master/cfe/include/lib_queue.h:
+/Users/easy/Downloads/cfe-master/cfe/include/lib_string.h:
+/Users/easy/Downloads/cfe-master/cfe/include/lib_malloc.h:
+/Users/easy/Downloads/cfe-master/cfe/include/lib_printf.h:
+/Users/easy/Downloads/cfe-master/cfe/include/lib_hssubr.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_iocb.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_device.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_ioctl.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_error.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_console.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_timer.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_devfuncs.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_cache.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_main.h:
+/Users/easy/Downloads/cfe-master/cfe/include/env_subr.h:
+/Users/easy/Downloads/cfe-master/cfe/include/ui_command.h:
+/Users/easy/Downloads/cfe-master/cfe/include/cfe_mem.h:
+/Users/easy/Downloads/cfe-master/cfe/include/exception.h:
+/Users/easy/Downloads/cfe-master/cfe/include/segtable.h:
+/Users/easy/Downloads/cfe-master/cfe/include/initdata.h:
